4d401fa855f5ef3fa5a6eeb11deb0b27a9399ed6
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-10-10 16:16:28 +0530

    new project

---
+++ app/src/main/java/com/example/harsh/iiitdquora/IIITDQuoraActivity.java
@@ -0,0 +1,13 @@
+package com.example.harsh.iiitdquora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IIITDQuoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iiitdquora);
+    }
+}
