565c441c3cc4fe69edb24accf1ab342ade6c15df
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-10-13 21:10:31 +0530

    action bar removed

--- app/src/main/java/com/example/harsh/iiitdquora/IIITDQuoraActivity.java
+++ app/src/main/java/com/example/harsh/iiitdquora/IIITDQuoraActivity.java
@@ -1,5 +1,6 @@
 package com.example.harsh.iiitdquora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,8 @@ public class IIITDQuoraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iiitdquora);
+        final Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(intent);
         //TODO: A lot of things
     }
 
