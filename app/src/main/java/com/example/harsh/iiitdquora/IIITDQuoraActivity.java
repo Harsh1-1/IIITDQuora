bae247e68318825d16a4c0876a25b4c7fbdbb04e
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-10-15 23:51:29 +0530

    action bar removed

--- app/src/main/java/com/example/harsh/iiitdquora/IIITDQuoraActivity.java
+++ app/src/main/java/com/example/harsh/iiitdquora/IIITDQuoraActivity.java
@@ -21,5 +21,4 @@ public class IIITDQuoraActivity extends AppCompatActivity {
         super.onStart();
     }
 
-
 }
---
+++ app/src/main/java/com/example/harsh/iiitdquora/UserProfileFragment.java
@@ -0,0 +1,43 @@
+package com.example.harsh.iiitdquora;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UserProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UserProfileFragment extends Fragment {
+
+    public UserProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment UserProfileFragment.
+     */
+    public static UserProfileFragment newInstance() {
+        UserProfileFragment fragment = new UserProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+
+}
