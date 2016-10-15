dcaf8221267b5d44088de1f03464d0df106a4036
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-10-15 14:32:36 +0530

    action bar removed

--- app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
+++ app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.harsh.iiitdquora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        //SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
