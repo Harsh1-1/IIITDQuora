0748e6a8a310d49b941c039ee19fa44b148ab05e
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-10-16 14:50:32 +0530

    User Profile added

--- app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
+++ app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.harsh.iiitdquora;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.view.*;
@@ -10,24 +12,49 @@ import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        fragmentManager=getSupportFragmentManager();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.home_menu, menu);
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
+        MenuItem searchItem = menu.findItem(R.id.QuestionSearch);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
         // Configure the search info and add any event listeners...
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.QuestionSearch:
+                //TODO
+                return true;
+
+            case R.id.UserProfile:
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.mainLayout, UserProfileFragment.newInstance());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                return true;
+
+            case R.id.Logout:
+                //TODO
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
