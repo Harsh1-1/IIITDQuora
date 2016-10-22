5701eafaf3ab228d746d8c501a3ed2c30865b301
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-10-22 13:27:32 +0530

    AskQuestionWork

--- app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
+++ app/src/main/java/com/example/harsh/iiitdquora/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.harsh.iiitdquora;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,10 +12,15 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
     FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,10 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         fragmentManager=getSupportFragmentManager();
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
     }
 
     @Override
@@ -66,6 +77,44 @@ public class HomeActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment retFragment = null;
+            switch (position){
+                case 1:
+                    retFragment = FeedFragment.newInstance();
+                    break;
+                case 2:
+                    retFragment = AskedFragment.newInstance();
+                    break;
+                case 3:
+                    retFragment = AnswerFragment.newInstance();
+                    break;
+            }
+            return retFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
         }
     }
 
