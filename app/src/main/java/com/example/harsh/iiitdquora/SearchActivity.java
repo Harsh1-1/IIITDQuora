9cd6d85c8c9d910efe6f8dfc79028a6eeaf6691e
Picasso056 <tushar16056@iiitd.ac.in>
2016-11-18 14:15:38 +0530

    view and add answers done

---
+++ app/src/main/java/com/example/harsh/iiitdquora/SearchActivity.java
@@ -0,0 +1,33 @@
+package com.example.harsh.iiitdquora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+
+    QuestionListAdapter questionListAdapter;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager recyclerLayoutManager;
+    ArrayList<Question>dataset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        questionListAdapter = new QuestionListAdapter(this);
+        recyclerView = (RecyclerView)findViewById(R.id.searchRecyclerView);
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        recyclerView.setAdapter(questionListAdapter);
+    }
+
+    public void update(ArrayList<Question> dataset){
+        this.dataset = dataset;
+        questionListAdapter.setDataset(dataset);
+    }
+}
