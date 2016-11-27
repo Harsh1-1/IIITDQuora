10f8b7f83e5fac9c738e769d2e0fc8b960607ed2
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-11-27 23:59:40 +0530

    small fixes

---
+++ app/src/main/java/com/example/harsh/iiitdquora/UpdateInterestTask.java
@@ -0,0 +1,102 @@
+package com.example.harsh.iiitdquora;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+public class UpdateInterestTask extends AsyncTask<String,String,String> {
+
+    Context ctx;
+
+    UpdateInterestTask(Context ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String retrieving_url = "http://onlyforgeeks.net16.net/iiitdquora/updateinterests.php";
+        String email = params[0];
+        String interests = params[1];
+        try {
+            URL url = new URL(retrieving_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            OutputStream OS = httpURLConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(OS,"UTF-8"));
+            String data = URLEncoder.encode("user_email","UTF-8") + "=" + URLEncoder.encode(email,"UTF-8")+ "&"
+                    +URLEncoder.encode("user_interests","UTF-8") + "=" + URLEncoder.encode(interests,"UTF-8");
+
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            OS.close();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+            String response = "";
+            String line = "";
+            while ((line = bufferedReader.readLine())!=null)
+            {
+                response = response + line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            return response;
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String[] server_response = result.split("@@@");
+        result=server_response[1];
+
+        if(result.equals("Failed to update interests"))
+        {
+            Toast.makeText(ctx,"Failed to update interests",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(ctx,"interests updated successfully",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
