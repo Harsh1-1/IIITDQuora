3f5d5cf8fa094ebbadfd7a44c5a57d63b8ffd8af
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-11-27 22:01:24 +0530

    small fixes

---
+++ app/src/main/java/com/example/harsh/iiitdquora/UnAnsweredQuestionsTask.java
@@ -0,0 +1,120 @@
+package com.example.harsh.iiitdquora;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+public class UnAnsweredQuestionsTask extends AsyncTask <String,String,String>{
+
+    Context ctx;
+
+    UnAnsweredQuestionsTask(Context ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String retrieving_url = "http://onlyforgeeks.net16.net/iiitdquora/unansweredquestion.php";
+        try {
+            URL url = new URL(retrieving_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+            String response = "";
+            String line = "";
+            while ((line = bufferedReader.readLine())!=null)
+            {
+                response = response + line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            return response;
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String[] server_response = result.split("@@@");
+        result=server_response[1];
+
+        if(result.equals("No unanswered Questions :D"))
+        {
+            Toast.makeText(ctx,"No unanswered Questions :D",Toast.LENGTH_LONG).show();
+        }
+        else if(result.equals("Failed to fetch questions"))
+        {
+            Toast.makeText(ctx,"Failed to fetch questions",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("server_response");
+                int count = 0;
+                int questionid;
+                String description,createdby,createdon,questiontext;
+                ArrayList<Question> questionArrayList = new ArrayList<>();
+                while (count<jsonArray.length())
+                {
+                    JSONObject JO = jsonArray.getJSONObject(count);
+                    questionid = JO.getInt("QuestionID");
+                    description = JO.getString("Description");
+                    createdby = JO.getString("Createdby");
+                    createdon = JO.getString("Createdon");
+                    questiontext = JO.getString("Questiontext");
+                    Question question = new Question(questionid,description,createdby,createdon,questiontext);
+                    questionArrayList.add(question);
+                    count++;
+                }
+                ((HomeActivity)ctx).updateAnswer(questionArrayList);
+            }
+
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+}
