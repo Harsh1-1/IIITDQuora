b34051ca3ba1ad0a4b704161aabccb3539074997
Picasso056 <tushar16056@iiitd.ac.in>
2016-11-27 14:41:47 +0530

    google sign in

--- app/src/main/java/com/example/harsh/iiitdquora/AuthenticateWithToken.java
+++ app/src/main/java/com/example/harsh/iiitdquora/AuthenticateWithToken.java
@@ -2,6 +2,16 @@ package com.example.harsh.iiitdquora;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 /**
  * Created by Tushar on 27-11-2016.
  */
@@ -9,6 +19,32 @@ import android.os.AsyncTask;
 public class AuthenticateWithToken extends AsyncTask<String, Void, Integer> {
     @Override
     protected Integer doInBackground(String... strings) {
+
+         String signinurl = "www.";
+        String tokenID = strings[0];
+
+        try {
+            URL url = new URL(signinurl);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+
+
+
+            httpURLConnection.disconnect();
+
+
+            return 0;
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
 
         return null;
