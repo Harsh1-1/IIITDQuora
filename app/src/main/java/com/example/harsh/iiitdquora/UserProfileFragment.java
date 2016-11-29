b5bd5ce11615fae15d4eb1556b265a5a4ac03827
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-11-29 10:07:14 +0530

    minor changes

--- app/src/main/java/com/example/harsh/iiitdquora/UserProfileFragment.java
+++ app/src/main/java/com/example/harsh/iiitdquora/UserProfileFragment.java
@@ -131,7 +131,7 @@ public class UserProfileFragment extends Fragment implements Updatable{
                     return;
                 }
                 ImageResizerTask imageResizerTask = new ImageResizerTask(imageView, this);
-                imageResizerTask.execute(SignInActivity.user.getPicurl(), "128", "128", "url");
+                imageResizerTask.execute(SignInActivity.user.getPicurl(), "256", "256", "url");
             }else imageView.setImageDrawable(null);
         }
     }
